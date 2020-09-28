c24f96803d3564e596d9952fdbb00a30a6eac673
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-09-28 08:17:19 -0600

    Deleted HttpRequest data

--- utgram/urls.py
+++ utgram/urls.py
@@ -1,22 +1,15 @@
+""" UTgram URls """
 
 from django.contrib import admin
 from django.urls import path
 
-from django.http import HttpResponse
 
 from posts import views as posts_views
 
 
-def hello_world(request):
-    return HttpResponse('''
-        <h1>Hola Mundo!</h1>
-        <h3>Hola desde django</h3>
-        ''')
-
-
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('hello-world/',hello_world),
+
 
     # Posts views
     path('feed/', posts_views.list_post)
