9fcc22b2a1b6c79858561beb3d76576220ad1389
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-09-28 08:44:08 -0600

    Static files config

--- utgram/urls.py
+++ utgram/urls.py
@@ -3,6 +3,9 @@
 from django.contrib import admin
 from django.urls import path
 
+# Static files config
+from django.conf import settings
+from django.urls.static import static
 
 from posts import views as posts_views
 
@@ -13,4 +16,4 @@ urlpatterns = [
 
     # Posts views
     path('feed/', posts_views.list_post)
-]
+] + static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
