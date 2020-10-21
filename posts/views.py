8b5549ca90d34b406af5a3df9de5a3e6d732e417
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-10-21 10:29:43 -0600

    Managin users accounts

--- posts/views.py
+++ posts/views.py
@@ -1,58 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-# Utilities
-from datetime import datetime
+# Models
+from posts.models import Post
 
+# Forms
+from posts.forms import PostForm
 
-posts = [
-    {
-        'title': 'Picture from Nietzy',
-        'user': {
-            'name': 'Nietzy Cardoza',
-            'picture': 'https://i.picsum.photos/id/404/60/60.jpg?hmac=lwzhD_dCmmfxmIzRzpe6zlpqaTDqkWYTB5WY--m-u6k'
-        },
-        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
-        'photo': 'https://i.picsum.photos/id/388/300/300.jpg?hmac=vnDPcudsMpVgI2d2TCqZEEaHnHczNJzfl04fQHpZL-A',
-    },
-    {
-        'title': 'Picture from Crecencio',
-        'user': {
-            'name': 'Crecencio Gameros',
-            'picture': 'https://i.picsum.photos/id/473/60/60.jpg?hmac=4UPx2kZtGolEBosWuZJBOgAkBpz_F8ttwQMz3AMd-BA'
-        },
-        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
-        'photo': 'https://i.picsum.photos/id/611/300/300.jpg?hmac=EJgvBzCx8e8CIwFeaaLBr3W56RSBPq8uOhwiEdeEMh0',
-    },
-    {
-        'title': 'Picture from Brenda',
-        'user': {
-            'name': 'Brenda Gutierrez',
-            'picture': 'https://i.picsum.photos/id/403/60/60.jpg?hmac=1mL7eZC4buqRuQYMqOw2Pdt74UW314X6pkaHbSBfXak'
-        },
-        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
-        'photo': 'https://i.picsum.photos/id/839/300/300.jpg?hmac=_3qtl641-KhyDcOcpQjTRl8MbB0sES_OBhau2yUoRAE',
-    },
-    {
-        'title': 'Picture from Luis',
-        'user': {
-            'name': 'Luis Hernandez',
-            'picture': 'https://i.picsum.photos/id/326/60/60.jpg?hmac=kqQqOQvh-TQO4lgvQO4ObvAcBuT7XxO4kKkcE9iuy3I'
-        },
-        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
-        'photo': 'https://i.picsum.photos/id/485/300/300.jpg?hmac=olQHiVzIoMWmV_hqK8mx7joT4g3QbQRUwebJ4yRVEx8',
-    },
-    {
-        'title': 'Picture from Vianey',
-        'user': {
-            'name': 'Vianey Lopez',
-            'picture': 'https://picsum.photos/60/60'
-        },
-        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
-        'photo': 'https://i.picsum.photos/id/743/300/300.jpg?hmac=etOxwclMseIaDm0uF76VFeu27d7MXfDkNVwZQkQ_KVI',
-    }
-]
 
 @login_required
 def list_post(request):
+    posts = Post.objects.all().order_by('-created')
     return render(request,'posts/feed.html',{'posts': posts})
+
+
+@login_required
+def create_post(request):
+    ''' Create a new post view '''
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+
+    else:
+        form=PostForm()
+
+    return render(
+        request=request,
+        template_name = 'posts/new.html',
+        context={
+            'form':form,
+            'user':request.user,
+            'profile':request.user.profile
+        }
+    )
--- utgram/urls.py
+++ utgram/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Views
+from django.contrib.auth import views as auth_views
 from posts import views as posts_views
 from users import views as users_views
 
@@ -17,13 +19,33 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Posts views
-    path('posts/feed/', posts_views.list_post, name='feed'),
+    path('', posts_views.list_post, name='feed'),
+    path('new/',posts_views.create_post, name='create_post'),
 
     # Users views
-    path('', users_views.login_view, name='login' ),
+    path('login/', users_views.login_view, name='login' ),
     path('logout/', users_views.logout_view, name='logout'),
     path('signup/', users_views.signup, name='signup'),
-    path('me/profile',users_views.update_profile, name='update_profile'),
+    path('me/profile/',users_views.update_profile, name='update_profile'),
+
+    # Restore password views
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete')
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
